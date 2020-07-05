4ff680f6b6436c856946a87061e121fcc5c02579
Julian Builes <julianbuilesc@gmail.com>
2020-07-04 23:50:30 -0500

    Template react

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom";
+import Template from "./components/Template";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDom.render(<Template />, document.getElementById("root"));
