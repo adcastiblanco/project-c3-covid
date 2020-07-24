832e251d576f97bd156096fa87708cb193aa407f
Luis LC <luis.lazcanocruz@gmail.com>
2020-07-24 15:28:06 -0500

    Login OAuth 2

---
+++ config/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const config = {
+  googleClientSecret : process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
+}
+
+export default config;
---
+++ utils/auth/strategies/oauth.js
@@ -0,0 +1,3 @@
+const passport = require('passport');
+const axios = require('axios');
+const { OAuth2Strategy } = require('passport-oauth');
