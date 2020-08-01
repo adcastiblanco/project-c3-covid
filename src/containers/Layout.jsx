a9423ae7e4a77f6fd61b8005f743ab0c5a20683c
Luis LC <luis.lazcanocruz@gmail.com>
2020-07-31 22:57:42 -0500

    Putting only a scss file at index.js

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import App from '../assets/styles/App.scss'
 
 const Layout = (props) => (
     <>
