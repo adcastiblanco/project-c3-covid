f975393b1f148234d8676164e776db36e0cde00b
adcastiblanco <angelcastiblanco96@gmail.com>
2020-08-06 18:47:21 -0500

    styles of dropmenu

--- src/components/Dashboard/DropDownMenu/index.js
+++ src/components/Dashboard/DropDownMenu/index.js
@@ -10,35 +10,33 @@ const DropDownMenu = ({ title, items = [], handleSetCountry, country }) => {
   };
 
   return (
-    <div className='dd-wrapper'>
+    <>
       <div
         tabIndex={0}
-        className='dd-header'
-        role='button'
+        className="dd-header"
+        role="button"
         onKeyPress={() => toggle(!open)}
         onClick={() => toggle(!open)}
       >
-        <div className='dd-header__title'>
-          <p className='dd-header__title--bold'>
-            { country || title }
-          </p>
+        <div className="dd-header__title">
+          <p className="dd-header__title--bold">{country || title}</p>
         </div>
-        <div className='dd-header__action'>
+        <div className="dd-header__action">
           <p>{open ? 'Close' : 'Open'}</p>
         </div>
       </div>
       {open && (
-        <ul className='dd-list'>
+        <ul className="dd-list">
           {items.map((item) => (
-            <li className='dd-list-item' key={item.alpha3Code}>
-              <button type='button' onClick={() => handledOnClick(item)}>
+            <li className="dd-list-item" key={item.alpha3Code}>
+              <button type="button" onClick={() => handledOnClick(item)}>
                 <span>{item.name}</span>
               </button>
             </li>
           ))}
         </ul>
       )}
-    </div>
+    </>
   );
 };
 
