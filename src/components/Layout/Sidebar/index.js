d61e00ba26c45184e767b826791a8e48b2473894
Luis LC <luis.lazcanocruz@gmail.com>
2020-08-11 16:40:42 -0500

    Adding responsive to sidebar

--- src/components/Layout/Sidebar/index.js
+++ src/components/Layout/Sidebar/index.js
@@ -1,31 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { Aside, SidebarList, SidebarItem } from './styles'
+import { Aside, SidebarList, SidebarItem } from './styles';
 
 const Sidebar = () => {
-    return (
-        <Aside>
-            <SidebarList>
-                <SidebarItem>
-                    <Link to="/">Estadisticas</Link>
-                </SidebarItem>
-                <SidebarItem>
-                    <Link to="/RegisterSymptoms">Registrar sintomas</Link>
-                </SidebarItem>
-                <SidebarItem>
-                    <Link to="/">Consejos de cuidado</Link>
-                </SidebarItem>
-                <SidebarItem>
-                    <Link to="/">Sobre el COVID-19</Link>
-                </SidebarItem>
-                <SidebarItem>
-                    <Link to="/">Acerca de nosotros</Link>
-                </SidebarItem>
-            </SidebarList>
-            <p><span>Platzi</span>Master</p>
-        </Aside>
-    )
-}
-
-export default Sidebar
+  return (
+    <Aside>
+      <SidebarList>
+        <SidebarItem>
+          <Link to='/'>Estadisticas</Link>
+        </SidebarItem>
+        <SidebarItem>
+          <Link to='/RegisterSymptoms'>Registrar sintomas</Link>
+        </SidebarItem>
+        <SidebarItem>
+          <Link to='/'>Consejos de cuidado</Link>
+        </SidebarItem>
+        <SidebarItem>
+          <Link to='/'>Sobre el COVID-19</Link>
+        </SidebarItem>
+        <SidebarItem>
+          <Link to='/'>Acerca de nosotros</Link>
+        </SidebarItem>
+      </SidebarList>
+      {/* <p>
+        <span>Platzi</span>Master
+      </p> */}
+    </Aside>
+  );
+};
+<p>
+<span>Platzi</span>Master
+</p>
+export default Sidebar;
--- src/components/Layout/Sidebar/styles.js
+++ src/components/Layout/Sidebar/styles.js
@@ -1,10 +1,11 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const Aside = styled.aside`
   background: #1d2129;
   grid-area: side;
   display: flex;
   flex-flow: column;
+  height: fit-content;
   justify-content: space-between;
   padding-bottom: 10px;
   box-shadow: 1px 1px 4px -1px #252a34;
@@ -18,32 +19,32 @@ export const Aside = styled.aside`
       color: #98ca3f;
     }
   }
-`
+`;
 
 export const SidebarList = styled.ul`
-    padding: 0;
-    width: 100%;
-    margin: 0;
-    list-style: none;
-`
+  padding: 0;
+  width: 100%;
+  margin: 0;
+  list-style: none;
+`;
 
 export const SidebarItem = styled.li`
- & a {
-      display: block;
-      line-height: 2.5em;
-      height: 2.5em;
-      text-align: center;
-      text-decoration: none;
-      color:#fbfbfb;
-      font-size: 1em;
-      font-weight: bold;
-      border-radius: 5px;
-      border: 1px solid transparent;
-      &:hover {
-        color: #08d9d6;
-        transition: 0.2s;
-        border: 1px solid #252a34;
-        background-color: #252a34;
-      }
+  & a {
+    display: block;
+    line-height: 2.5em;
+    height: 2.5em;
+    text-align: center;
+    text-decoration: none;
+    color: #fbfbfb;
+    font-size: 1em;
+    font-weight: bold;
+    border-radius: 5px;
+    border: 1px solid transparent;
+    &:hover {
+      color: #08d9d6;
+      transition: 0.2s;
+      border: 1px solid #252a34;
+      background-color: #252a34;
     }
-`
+  }
+`;
