796725294665920e2a3311165d5c484d81de0182
Luis LC <luis.lazcanocruz@gmail.com>
2020-08-11 13:57:36 -0500

    Createn a style file to dashboard

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,21 +1,20 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Layout from '../components/Layout'
-import Home from '../containers/Home'
-import LoginRegister from '../containers/LoginRegister'
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Layout from '../components/Layout';
+import Home from '../containers/Home';
+import LoginRegister from '../containers/LoginRegister';
 import RegisterSymptoms from '../containers/RegisterSymptoms';
-// import device from '../assets/styles/Breakpoints';
 
 const App = () => (
-    <BrowserRouter>
-        <Switch>
-            <Route exact path="/login" component={LoginRegister} />
-            <Layout>
-                <Route exact path="/RegisterSymptoms" component={RegisterSymptoms} />
-                <Route exact path="/" component={Home} />
-            </Layout>
-        </Switch>
-    </BrowserRouter>
-)
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/login' component={LoginRegister} />
+      <Layout>
+        <Route exact path='/RegisterSymptoms' component={RegisterSymptoms} />
+        <Route exact path='/' component={Home} />
+      </Layout>
+    </Switch>
+  </BrowserRouter>
+);
 
-export default App
+export default App;
