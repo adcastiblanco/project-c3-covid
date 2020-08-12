da07a768f077f1ec8c7697ad895f9d5e21c70a61
Angel Castiblanco <angelcastiblanco96@gmail.com>
2020-08-12 12:56:25 -0500

    Bugs fixed

--- src/routes/App.js
+++ src/routes/App.js
@@ -3,9 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Layout from '../components/Layout';
 import Home from '../containers/Home';
-import CareTips from '../containers/CareTips'
-import AboutUs from '../containers/AboutUs'
-import AboutCovid from '../containers/AboutCovid'
+
+import '../assets/styles/Main.scss'
 
 import LoginRegister from '../containers/LoginRegister';
 import RegisterSymptoms from '../containers/RegisterSymptoms';
@@ -20,9 +19,6 @@ const App = () => (
       <Layout>
         <Route exact path='/' component={Home} />
         <Route exact path='/RegisterSymptoms' component={RegisterSymptoms} />
-        <Route exact path='/caretips' component={CareTips} />
-        <Route exact path='/about-us' component={AboutUs} />
-        <Route exact path='/about-covid' component={AboutCovid} />
       </Layout>
     </Switch>
   </BrowserRouter>
