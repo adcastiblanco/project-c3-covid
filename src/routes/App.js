7727d75a1706802e3b1f310af4bcb02cb7e86bd8
Julian Builes <julianbuilesc@gmail.com>
2020-08-13 21:40:33 -0500

    Add function SecureRoutes, this validate that user has loged

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Layout from '../components/Layout';
 import Home from '../containers/Home';
 import LoginRegister from '../containers/LoginRegister';
@@ -7,14 +7,28 @@ import RegisterSymptoms from '../containers/RegisterSymptoms';
 import GlobalStyles from '../assets/styles/GlobalStyles';
 import '../assets/styles/Main.scss';
 
+function SecureRoutes({ component, path, ...restElements }) {
+  const token = localStorage.getItem('token');
+  const username = localStorage.getItem('username');
+  if (token && username) {
+    return <Route path={path} component={component} {...restElements} />;
+  } else {
+    return <Redirect to="/login" {...restElements} />;
+  }
+}
+
 const App = () => (
   <BrowserRouter>
     <GlobalStyles />
     <Switch>
-      <Route exact path='/login' component={LoginRegister} />
+      <Route exact path="/login" component={LoginRegister} />
       <Layout>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/RegisterSymptoms' component={RegisterSymptoms} />
+        <SecureRoutes exact path="/" component={Home} />
+        <SecureRoutes
+          exact
+          path="/RegisterSymptoms"
+          component={RegisterSymptoms}
+        />
       </Layout>
     </Switch>
   </BrowserRouter>
